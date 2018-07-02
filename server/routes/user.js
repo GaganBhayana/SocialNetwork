acca0a128f57ddffa1e0d9ba280d67e588807511
Rohit Bokade <bokaderohit98@gmail.com>
2018-07-02 11:36:43 +0530

    UPDATED: /user routes and added authenticate middleware

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,3 +1,4 @@
+//LOADING MODELS
 const User = require('../models/user');
 const Comment = require('../models/comment');
 const Group = require('../models/group');
@@ -5,180 +6,130 @@ const Page = require('../models/page');
 const Post = require('../models/page');
 const Notification = require('../models/notification');
 
+
+//LOADING DEPENDENCIES
 const express = require('express');
-var querystring = require('querystring');
 const router = express.Router();
+const authenticate = require('../helpers/authenticate');
 
-const fixed = 10;
+/***********************************************************
+                        ROUTES
+***********************************************************/
 
+//FETCHING DETAILS OF A USER
+router.get('/', authenticate, (req, res) => {
+  let id = null;
 
-// get Requests--------------------------------
-router.get('/',(req,res)=>{// dashboard
-    User.find({_id:req.user._id})
-    .then((user)=>{
-        // user.friends
-    }) 
-})
+  if (!req.query.id) {
+    res.status(200)
+      .json(req.user);
+  } else {
+    id = req.query.id;
+  }
 
-//profile of a user - anyone logged in can see it
-router.get('/:id',(req,res) => {
-    // res.send("this is the user"+req.params.id);
-    User.find({_id:req.params.id})
-    .then((user)=>{
-        user.posts.find({}).sort({date:-1})
-        .limit(req.query.count)
+  User.findById(id)
+    .then(user => {
+      if (user.friends.indexOf(req.user._id) === -1) {
+        user.isFriend = false;
+      } else {
+        user.isFriend = true;
+      }
+      res.status(200)
+        .json(user);
     })
-    .then((userPosts)=>{
-        res.status(200)
-            .json(userPosts);
-    })
-    .catch((err)=>{
-        res.status(404)
+    .catch(err => {
+      console.log(err);
+      res.status(500)
         .send();
-        console.log(err);
-    })
-}) 
+    });
+});
 
-// request for getting all the posts (profile page)
-router.get('/posts',(req,res) => {
-    // res.send("these are your posts");
-    User.find({_id:req.user.id})
-    .then((user)=>{
-        user.posts.find({}).sort({date:-1})
-        .limit(req.query.count);
+
+//FETCHING FRIEND LIST
+router.get('/friends', authenticate, (req, res) => {
+  let id = null;
+
+  if (req.query.id) {
+    id = req.query.id;
+  } else {
+    id = req.user._id;
+  }
+
+  User.findById(id)
+    .then(user => {
+      return User.find({
+        _id: {
+          $in: user.friends
+        }
+      });
     })
-    .then((posts)=>{
-        res.status(200)
-            .json(posts);
+    .then(friends => {
+      res.status(200)
+        .json(friends);
     })
-    .catch((err)=>{
-        res.status(404)
+    .catch(err => {
+      console.log(err);
+      res.status(500)
         .send();
-        console.log(err);
     })
-})
+});
 
-// request for getting the list of all friends
-router.get('/friends',(req,res) => {
-    // res.send("these are your friends");
-    User.find({_id:req.user._id})
-    .then((user)=>{
-        user.friends.find({})
-        .limit(req.query.count);
+
+//FETCHING FRIEND REQUESTS
+router.get('/friend-requests', authenticate, (req, res) => {
+  User.find({
+    _id: {
+      $in: req.user.friendRequests
+    }
+  })
+    .then(friendRequests => {
+      res.status(200)
+        .json(friendRequests);
     })
-    .then((friends)=>{
-        res.status(200)
-            .json(friends);
-    })
-    .catch((err)=>{
-        res.status(404)
+    .catch(err => {
+      console.log(err);
+      res.status(500)
         .send();
-        console.log(err);
-    })
-})
+    });
+});
 
 
-// post requets -------------------------------------------
-// registereing a new user
-router.post('/',(req,res)=>{
-    var user= {
-        email: req.body.email,
-        name: req.body.name,
-        tagLine: (req.body.tagLine)?req.body.tagLine:"",
-        img: " ",
-        role: req.body.role,
-        gender: req.body.gender,
-        dob: req.body.dob,
-        location: req.body.location ? req.body.location :"",
-        school: req.body.school ? req.body.school: ""
-    }  
-    User.insert({
-        email:user.email, 
-        name:user.name, 
-        tagLine:user.tagLine,
-        role: user.role,
-        gender: user.gender,
-        dob: user.dob,
-        location: user.location,
-        school: user.school
-    })
-    .then(()=>{
-        res.status(200);
-        consols.log("User added successfully");
-    })
-    .catch((err)=>{
-        res.status(404)
+//UPDATING USER PROFILE
+router.put('/', authenticate, (req, res) => {
+  delete req.body.email;
+  delete req.body.password;
+
+  User.findByIdAndUpdate(req.user._id, req.body)
+    .then(() => {
+      res.status(200)
         .send();
-        console.log('error occured: '+err);
     })
-})
-
-// making a new post
-// router.post('/posts',(req,res)=>{
-//     var post = {
-//         title: req.body.title,
-//         content: req.body.content,
-//         img: "",
-//         owner: req.params.id,
-//         date: req.body.date
-//     }
-//     User.find({_id:req.user._id})
-//     .then((user)=>{
-//         user.insert({
-//             title: user.title,
-//             content: user.content,
-//             img: user.img,
-//             owner: user.owner,
-//             date: user.date
-//         })
-//     })
-//     .then(()=>{
-//         res.status(200);
-//         console.log('Post added successfully');
-//     })
-//     .catch((err)=>{
-//         res.status(404)
-//         .send();
-//         console.log(err);
-//     })
-// })
-
-// delete requests
-// thought: put the deleted user in temp database which can be recovered within 30 days if needed
-// removing the user 
-router.delete('/',()=>{
-    User.remove({_id:req.user._id})
-    .then((user)=>{
-        res.status(200);
-        console.log("User removed successfully!");
-    })
-    .catch((err)=>{
-        res.status(404)
+    .catch(err => {
+      console.log(err);
+      res.status(500)
         .send();
-       console.log("User not removed!"); 
+    });
+});
+
+
+//DELETING USER PROFILE
+// TODO: ADD THE DELETED USER IN A DELETED USER MODEL SO THAT THE ACCOUNT
+//CAN BE RECOVERED WITHIN 30 DAYS.
+router.delete('/', authenticate, (req, res) => {
+  User.findById(req.user._id)
+    .then(user => {
+      user.remove();
     })
-})
-
-//***************** Also write code to remove from the collection of post,page and group */
-
-//  update requests---------------------------------------
-
-// updating user details
-router.put('/',(req,res)=>{
-    let newUser = req.body;
-    User.findByIdAndUpdate(req.user._id,newUser)
-    .then(()=>{
-        res.status(200);
-        console.log('User details updated successfully!')
-    })  
-    .catch((err)=>{
-        res.status(404)
+    .then(() => {
+      res.status(200)
         .send();
-        console.log(err+" occured");
     })
-})
-
-
+    .catch(err => {
+      console.log(err);
+      res.status(500)
+        .send()
+    });
+});
 
 
 module.exports = router;
