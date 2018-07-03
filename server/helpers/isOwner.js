a5193620b3bedbafed6ff301b951893369354f6b
GaganBhayana <gagan2oct1998@gmail.com>
2018-07-03 14:16:43 +0530

    owner verification - page

--- server/helpers/isOwner.js
+++ server/helpers/isOwner.js
@@ -2,6 +2,7 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
 const Group = require('../models/group');
+const Page = require('../models/page');
 
 
 /*******************************************************
@@ -46,6 +47,23 @@ module.exports.isGroupOwner = (req, res, next) => {
   Group.findById(req.params.id)
     .then(group => {
       if (group && group.owner.equals(req.user._id)) {
+        next();
+      } else {
+        res.status(400)
+          .send();
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500)
+        .json(err);
+    });
+};
+
+module.exports.isPageOwner = (req, res, next) => {
+  Page.findById(req.params.id)
+    .then(page => {
+      if (page && page.owner.equals(req.user._id)) {
         next();
       } else {
         res.status(400)
